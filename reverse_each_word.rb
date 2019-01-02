ccdf23e5cf33bfb3ccbbcf319be1bb1a9408b14c
Calie Rushton <calielrushton@gmail.com>
2019-01-02 18:21:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,8 @@ def reverse_each_word(string)
   string.split(" ").each { |word| reversed << word.reverse }
   reversed.join(" ")
 end
+
+def reverse_each_word(string)
+  string.split(" ").collect { |word| word.reverse }
+  .join(" ")
+end
